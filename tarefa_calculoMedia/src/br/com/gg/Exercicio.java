3947449b541219e5a2288d6f9847ff6b19495fc5
guistupp <guistiipp@gmail.com>
2024-04-22 20:50:02 -0300

    commit

---
+++ tarefa_calculoMedia/src/br/com/gg/Exercicio.java
@@ -0,0 +1,17 @@
+package br.com.gg;
+
+public class Exercicio {
+
+	public static void main(String[] args) {
+
+		double num1 = 7.0;
+		double num2 = 7.0;
+		double num3 = 9.0;
+		double num4 = 8.0;
+		
+		double div = (num1 + num2 + num3 + num4) / 4;
+		
+		System.out.println(div);
+	}
+
+}
